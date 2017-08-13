f02933b56b7aec76d07be30a04e604027f9908b7
novakov_alex <novakov.alex@gmail.com>
2017-08-13 22:18:07 +0200

    simple grouping by nationality

--- src/main/scala/xml/analytics/Reports.scala
+++ src/main/scala/xml/analytics/Reports.scala
@@ -1,22 +1,27 @@
 package xml.analytics
 
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.functions._
 
 object Reports extends App {
-  //  val conf = new SparkConf().setAppName("WikiInfoboxReports").setMaster("local[*]")
-  //  val sc = new SparkContext(conf)
-
   val spark = SparkSession.builder().appName("WikiInfoboxReports").config("spark.master", "local[*]").getOrCreate()
 
   val personDf = spark.read.options(Map(
     "header" -> "true",
     "ignoreLeadingWhiteSpace" -> "true",
     "ignoreTrailingWhiteSpace" -> "true",
-    //    "timestampFormat" -> "yyyy-MM-dd HH:mm:ss.SSSZZZ",
-    "inferSchema" -> "true",
-    "mode" -> "FAILFAST"))
-    .csv("/Users/alexey/Downloads/wikipedia/output/person.csv")
+    "inferSchema" -> "true" //,
+    //    "mode" -> "FAILFAST"
+    //    "mode" -> "DROPMALFORMED"
+  )).csv("/Users/alexey/Downloads/wikipedia/output/person.csv")
 
-  personDf.show(truncate = false, numRows = 100)
+  val nationality = personDf.col("nationality")
+  val nationalityProjection = when(
+    nationality
+      .isin("American", "United States", "U.S.", "[[United States|American]]", "[[Americans|American]]", "[[United States]]", "USA", "{{USA}}"),
+    "American")
+    .otherwise(nationality)
+    .as("nationality_new")
+
+  personDf.groupBy(nationalityProjection).count().orderBy("count").show(truncate = false, numRows = 100)
 }
