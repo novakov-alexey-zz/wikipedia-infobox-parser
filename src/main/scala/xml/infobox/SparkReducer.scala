8a4d1c917c0aaed128fad3018c68614d701bb7b4
novakov_alex <novakov.alex@gmail.com>
2017-10-02 08:05:07 +0200

    use Spark to handle multiple files properly

--- src/main/scala/xml/infobox/SparkReducer.scala
+++ src/main/scala/xml/infobox/SparkReducer.scala
@@ -3,7 +3,6 @@ package xml.infobox
 import java.io.{File, FileWriter}
 import java.nio.file.Files
 
-import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -33,35 +32,26 @@ object SparkReducer extends App {
   val conf = new SparkConf().setAppName("WikiInfoBoxReducer").setMaster("local[*]")
   val sc = new SparkContext(conf)
 
-  val files = FileSystem.get(sc.hadoopConfiguration).listStatus(new Path(inputLocation.toString))
   Files.createDirectories(outputLocation.toPath)
 
   val sizeOfParallelChunk = 8
   val minimumPropertiesCount = 1
 
   // Process files
-  val groupedFiles = files.grouped(sizeOfParallelChunk).toSeq
-  groupedFiles.par.foreach(chunk => chunk.foreach { f =>
-    val csv = sc.wholeTextFiles(f.getPath.toString)
-      .map { case (path, content) =>
-        val parsedProps = InfoBoxPropertiesParser.parse(content)
-        if (parsedProps.size > minimumPropertiesCount) {
-          val pageId = InfoBoxes.pathKey -> path.split("/").last.split("\\.").head
-          val values = (infoBoxProps ++ parsedProps.filterKeys(k => infoBoxProps.contains(k)) + pageId).values
-          Some(values.mkString(","))
-        } else None
-      }
-      .collect()
-      .flatten
 
-    require(csv.length == 1, s"File ${f.getPath.toString} should have one Infobox")
-
-    csv.foreach { content =>
-      val writer = new FileWriter(s"$outputLocation/${f.getPath.getName}")
+  sc.wholeTextFiles(inputLocation.toString)
+    .map { case (path, content) => path -> InfoBoxPropertiesParser.parse(content) }
+    .filter { case (_, parsedProps) => parsedProps.size > minimumPropertiesCount }
+    .map { case (path, parsedProps) =>
+      val pageId = InfoBoxes.pathKey -> path.split("/").last.split("\\.").head
+      val values = (infoBoxProps ++ parsedProps.filterKeys(k => infoBoxProps.contains(k)) + pageId).values
+      pageId._2 -> values.mkString(",")
+    }
+    .foreach { case (pageId, content) =>
+      val writer = new FileWriter(s"$outputLocation/$pageId.csv")
       writer.write(content)
       writer.close()
     }
-  })
 
   sc.stop()
 }
